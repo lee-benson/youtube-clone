17e3ca09af34fceb98c5739b53651da6b6b3fee9
Benson Lee <BensonL1334@gmail.com>
2024-09-27 00:44:38 -0400

    Added ffmpeg to process videos to 360p

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,34 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
+
 
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World")
+app.post("/process-video", (req, res) => {
+  // Need to get the input video file from the request
+  const inputFilePath = req.body.inputFilePath; // Local dev
+  const outputFilePath = req.body.outputFilePath; // For cloud
+
+
+  // Need appropriate handling of errors if the above parameters are not given
+  if (!inputFilePath || !outputFilePath) {
+    res.status(400).send("Bad Request: Missing file path.");
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360") // 360p
+    .on("end", () => {
+      res.status(200).send("Processing finished successfully.")
+    })
+    .on("error", (err) => {
+      console.log(`An error occurred: ${err.message}`);
+      res.status(500).send(`Internal server error: ${err.message}`);
+    })
+    .save(outputFilePath);
 });
 
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Video processing service listening at http://localhost:${port}`)
 });
