c22461c2ae2d50dcaa27da3c30bf7a4e4e911e3f
Benson Lee <BensonL1334@gmail.com>
2024-10-19 03:35:08 -0400

    Upload Processed Video to GCS

--- video-processing-service/src/storage.ts
+++ video-processing-service/src/storage.ts
@@ -54,3 +54,20 @@ export async function downloadRawVideo(fileName: string) {
     `gs://${rawVideoBucketName}/${fileName} downloaded to ${localRawVideoPath}/${fileName}.`
   )
 }
+
+/**
+ * @param fileName - The name of the file to be uploaded from the 
+ * {@link localProcessedVideoPath} folder into the {@link processedVideoBucketName}.
+ *  @returns A promise that resolves when the file has been uploaded.
+ * */
+export async function uploadProcessedVideo(fileName: string) {
+  const bucket = storage.bucket(processedVideoBucketName);
+
+  await bucket.upload(`${localProcessedVideoPath}/${fileName}`), {
+    destination: fileName
+  };
+  console.log(
+    `${localProcessedVideoPath}/${fileName} uploaeded to gs://${processedVideoBucketName}/${fileName}`
+  );
+  await bucket.file(fileName).makePublic();
+}
