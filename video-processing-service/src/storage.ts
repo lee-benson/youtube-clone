cf772ac981b1dcdcb593ef180f72bd5db9db5023
Benson Lee <BensonL1334@gmail.com>
2024-10-19 04:03:01 -0400

    Delete Raw and Processed Videos from Local

--- video-processing-service/src/storage.ts
+++ video-processing-service/src/storage.ts
@@ -1,6 +1,7 @@
 import { Storage } from "@google-cloud/storage";
 import fs from "fs";
 import ffmpeg from "fluent-ffmpeg";
+import { log } from "console";
 
 // This file keeps track of both local and GCS 
 
@@ -70,4 +71,46 @@ export async function uploadProcessedVideo(fileName: string) {
     `${localProcessedVideoPath}/${fileName} uploaeded to gs://${processedVideoBucketName}/${fileName}`
   );
   await bucket.file(fileName).makePublic();
+}
+
+
+/**
+ * @params fileName - The name of the file to delete from the
+ * {@link localRawVideoPath} folder.
+ * @returns A promise that resolves when the file has been deleted.
+ *  */
+export function deleteRawVideo(fileName: string) {
+  return deleteFile(`${localRawVideoPath}/${fileName}`);
+}
+
+/**
+ * @params fileName - The name of the file to delete from the
+ * {@link localProcessedVideoPath} folder.
+ * @returns A promise that resolves when the file has been deleted.
+ *  */
+export function deleteProcessedVideo(fileName: string) {
+  return deleteFile(`${localProcessedVideoPath}/${fileName}`);
+}
+
+/**
+ * @param filePath - The path of the file to delete.
+ * @returns A promise that resolves when the file has been deleted.
+ *  */
+function deleteFile(filePath: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    if (fs.existsSync(filePath)) {
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.log(`Failed to delete file at ${filePath}`, err);
+          reject(err);
+        } else {
+          console.log(`File deleted at ${filePath}`);
+          resolve();
+        }
+      })
+    } else {
+      console.log(`File not found at ${filePath}, skipping the delete.`);
+      resolve();
+    }
+  });
 }
