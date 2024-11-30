3a41abc273e1218ab0745d2d1dedd6c56450ef95
Benson Lee <BensonL1334@gmail.com>
2024-11-30 02:21:33 -0500

    Add signin

--- yt-web-client/src/app/navbar/navbar.tsx
+++ yt-web-client/src/app/navbar/navbar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./navbar.module.css";
+import SignIn from "./sign-in";
 
 export default function Navbar() {
   return (
@@ -9,6 +10,7 @@ export default function Navbar() {
         <Image width={90} height={20}
           src="/youtube-logo.svg" alt="Youtube Logo" />
       </Link>
+      <SignIn />
     </nav>
   );
 }
