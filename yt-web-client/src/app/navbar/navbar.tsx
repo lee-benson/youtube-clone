08bd82f1039be3b95f3a51cc1de17cdef7fff0ad
Benson Lee <BensonL1334@gmail.com>
2024-12-05 02:05:12 -0500

    Sign in functionality added to navbar

--- yt-web-client/src/app/navbar/navbar.tsx
+++ yt-web-client/src/app/navbar/navbar.tsx
@@ -1,16 +1,33 @@
+'use client';
+
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./navbar.module.css";
 import SignIn from "./sign-in";
+import { onAuthStateChangedHelper } from "../firebase/firebase"
+import { useState, useEffect } from "react";
+import { User } from 'firebase/auth';
+
 
 export default function Navbar() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  });
+
+
   return (
     <nav className={styles.nav}>
       <Link href="/">
         <Image width={90} height={20}
           src="/youtube-logo.svg" alt="Youtube Logo" />
       </Link>
-      <SignIn />
+      <SignIn user={user} />
     </nav>
   );
 }
